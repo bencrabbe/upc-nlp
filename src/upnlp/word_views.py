3106880263b1be24f279d86d29f0b48073d9a402
Benoît Crabbé <bcrabbe@MacBook-Pro-de-SIM.local>
2025-01-03 18:47:02 +0100

    improved visualisations

--- src/upnlp/word_views.py
+++ src/upnlp/word_views.py
@@ -5,26 +5,34 @@ import pandas as pa
 # UMAP is a dimensionality reduction method like T-SNE but it scales up to larger data sets
 from umap import UMAP
 
+
+__all__ = ['emb2df','wn2df']
+
 """
 This module provides prototype functions for dumping word embeddings and wordnets to file compatible
 with large network analysis and visualisation software like networkX and cosmograph
 """
 
-def kv2dataframe(kv):
+def emb2df(kv,filter_list=None):
     """
-    Args:
-        kv (KeyedVectors): a word embedding object from the gensim library
+    This function projects n-dimensional embeddings to 2D and returns a dataframe mapping word strings to 2d vectors
 
+    Args:
+        kv (KeyedVectors) : a word embedding object from the gensim library
+        filter_list (list): list of strings, if not None, the output will have a vocabulary that results from
+                             the intersection with this list.
     Returns:
         a dataframe
     """
-    #WARNING : this function is likely to be compute intensive !
+    if filter_list:
+        filter_list = set(filter_list)
+        vocabulary = [wf for wf in kv.index_to_key if wf in filter_list]
 
-    embeddings   = kv[kv.index_to_key]
+    embeddings   = kv[vocabulary]
     embeddings2D = UMAP(n_components=2).fit_transform(embeddings)
-    return pa.DataFrame({'label'=kv.index_to_key,'x'=embeddings2D[:,0],'y'=embeddings2D[:,1]})
+    return pa.DataFrame({'id':vocabulary,'x':embeddings2D[:,0],'y':embeddings2D[:,1]})
 
-def wn2dataframe(wordnet):
+def wn2df(wordnet):
     """
     Args:
         wordnet (wn.Wordnet): a Wordnet instance
@@ -48,13 +56,12 @@ def wn2dataframe(wordnet):
 if __name__ == '__main__':
     import gensim.downloader
 
-    print(list(gensim.downloader.info()['models'].keys()))
-    df = kv2dataframe(gensim.downloader.load('glove-wiki-gigaword-100'))
+    wn_id             = 'oewn:2024'
+    wordnet = wn.Wordnet(wn_id)
+
+    df = emb2df(gensim.downloader.load('glove-wiki-gigaword-100'),filter_list= [word.lemma() for word in wordnet.words()])
     df.to_csv('glove.tsv',sep='\t')
 
-
-    wn_id             = 'oewn:2024'
-    wordnet           = wn.Wordnet(wn_id)
-    edges_df,nodes_df = wn2dataframe(wordnet)
+    edges_df,nodes_df = wn2df(wordnet)
     edges_df.to_csv(f'{wn_id}_edges.tsv',sep='\t')
     nodes_df.to_csv(f'{wn_id}_nodes.tsv',sep='\t')
